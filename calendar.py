5246b19b550b8d7e7aa0a7bd090c174158806e69
Noah Smith <bigbass98@gmail.com>
2018-10-02 00:12:20 -0500

    MonthList
    Created a list of the months to replace the strings in the if/then.

--- calendar.py
+++ calendar.py
@@ -20,24 +20,27 @@
 import math
 
 #greeting -
-print('Hey there and welcome to the calendar program!')
+print('Hey there and welcome to the calendar program! ')
 
 
 #var list
 
-year = int(input('What year are you looking for. 1800-2099'))
+year = int(input('What year are you looking for. 1800-2099 '))
 
 century_digits = int(str(year)[:2])
 
 year_digits = int(str(year)[-2:])
 
-month = input('Please input a month. ex: Jan, Feb, Mar')
-
-day = int(input('What day fool?'))
+month = input('Please input a month. ex: Jan, Feb, Mar ')
 
 value = year_digits + math.floor(year_digits/4)
 
-print('After year/4', value)
+
+monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
+
+print('After year/4 ', value)
 
 #Century Value Calulations
 if century_digits == 18:
@@ -45,51 +48,60 @@ if century_digits == 18:
 elif century_digits == 20:
     value = value + 6
 
-print('After cent calc', value)
+print('After cent calc ', value)
 
 #Month Value Calculations
-while month == None:
-    if month == 'Jan' and (year%4 != 0):
+
+while month != monthList:
+    if input(month) == monthList[0] and (year%4 != 0):
         value=+1
         break
-    elif month == 'Feb' and (year%4 == 0):
+    elif month == monthList[1] and (year%4 == 0):
         value=+3
         break
-        if month == 'Feb' and (year%4 != 0):
-         value=+4
-         break
-    elif month == 'Mar' or 'Nov':
+        if month == monthList[1] and (year%4 != 0):
+            value=+4
+            break
+    elif month == monthList[2] or monthList[10]:
         value=+4
         break
-    elif month == 'Apr' or 'Jul':
-        value = value + 0
+    elif month == monthList[3] or monthList[6]:
+        value=+0
         break
-    elif month == 'May':
-        value = value + 2
+    elif month == monthList[4]:
+        value=+2
         break
-    elif month == 'Jun':
-        value = value + 5
+    elif month == monthList[5]:
+        value=+5
         break
-    elif month == 'Aug':
-        value = value + 3
+    elif month == monthList[7]:
+        value=+3
         break
-    elif month == 'Oct':
-        value = value + 1
+    elif month == monthList[9]:
+        value=+1
         break
-    elif month == 'Sep' or 'Dec':
-        value = value + 6
+    elif month == monthList[8] or monthList[11]:
+        value=+6
         break
     else:
         print('Why did you not give me a correct answer? I thought I was clear enough.')
 
+
+
+day = int(input('What day fool? '))
+
+
         
-print('After month calc', value)
+print('After month calc ', value)
+
+
+
 
 value = value + day
-print('After day calc', value)
+print('After day calc ', value)
 total = value%7
 
-print ('Total', total)
+print ('Total ', total)
 if total == 1:
     print('The day is Sunday')
 elif total == 2:
@@ -104,5 +116,9 @@ elif total == 6:
     print('The day is Friday')
 elif total == 0:
     print('The day is Saturday')
+
+
+
+
 
 
