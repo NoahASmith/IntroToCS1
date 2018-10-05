25f9bc88a489fc73e2fb4884aabee16524a01085
Noah Smith <bigbass98@gmail.com>
2018-10-05 16:29:19 -0500

    Format
    Adding a little bit to the formatting

--- calendar.py
+++ calendar.py
@@ -115,3 +115,60 @@ elif value == 6:
     print('The day is Friday')
 elif value == 0:
     print('The day is Saturday')
+
+
+
+
+
+#Format!
+
+
+headJan = print(format('-', '-<10'), 'January', format('-', '-<10'))
+
+headFeb = print(format('-', '-<10'), 'February', format('-', '-<10'))
+
+headMar = print(format('-', '-<10'), 'March', format('-', '-<10'))
+
+headApr = print(format('-', '-<10'), 'April', format('-', '-<10'))
+
+headMay = print(format('-', '-<10'), 'May', format('-', '-<10'))
+
+headJun = print(format('-', '-<10'), 'June', format('-', '-<10'))
+
+headJul = print(format('-', '-<10'), 'July', format('-', '-<10'))
+
+headAug = print(format('-', '-<10'), 'August', format('-', '-<10'))
+
+headSep = print(format('-', '-<10'), 'September', format('-', '-<10'))
+
+headOct = print(format('-', '-<10'), 'October', format('-', '-<10'))
+
+headNov = print(format('-', '-<10'), 'November', format('-', '-<10'))
+
+headDec = print(format('-', '-<10'), 'December', format('-', '-<10'))
+
+
+startSun = '[1][2][3][4][5][6][7]'
+startMon = '[][1][2][3][4][5][6]'
+startTue = '[][][1][2][3][4][5]'
+startWed = '[][][][1][2][3][4]'
+startThu = '[][][][][1][2][3]'
+startFri = '[][][][][][1][2]'
+startSat = '[][][][][][][1]'
+
+
+
+
+
+print(headJan)
+print(startSun)
+
+
+
+
+
+
+
+
+
+
