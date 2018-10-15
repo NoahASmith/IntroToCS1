9ed5e6f6da26742e4ca57e12ddf2335ccd474ff5
Noah Smith <bigbass98@gmail.com>
2018-10-15 18:39:19 -0500

    New File/Lab!
    New treasure hunt lab. First edit is the steps.
    
    Always starting with the greeting!

---
+++ treasurehunt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Noah Smith Gunnar Nick
+# Team Slytherin
+
+#Lab 3  - Treasure Hunt
+
+
+#Add a greeting.
+#prompt for difficulty
+    # 3x3, 4x4, 5x5
+    
+#RANDOMLY place treasure on grid
+    #should be with two random integers in a list
+    
+#print where the user has already guessed
+    
+#Prompt when the user has guessed where the box is
+    
+#check if box is there
+    
+#check to see if there are any guesses left
